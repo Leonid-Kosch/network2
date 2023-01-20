a085c96a7fcb58a5aa5d4e501fe76b474bb4eb01
Leonid <leonidkoscheev984@gmail.com>
2023-01-20 19:49:35 +0100

    added every component in app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Dialogs from './components/Dialogs/Dialogs';
 import Header from './components/Header/Header';
+import Nav from './components/Nav/Nav';
+import Profile from './components/Profile/Profile';
 
 function App() {
   return (
-    <Header />
+    <Header />,
+    <Nav />,
+    <Profile />,
+    <Dialogs />
   );
 }
 
